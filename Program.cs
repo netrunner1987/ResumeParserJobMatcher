22df24383839ff2c03a306d8f1e389c502d12bef
netrunner1987 <mclinton@gmail.com>
2025-06-24 20:04:28 -0700

    Update Program.cs
    Updated Swashbuckler.AspNetCore to 9.0.1 and added package to Program.cs so that Swagger could work

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapGet("/weatherforecast", () => "Resume Parser Job Matcher API is running...");
